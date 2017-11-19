168930b54cdea26a4955d0ff1099377ae01dd24c
Sabrina John <sabrina.john1990@gmail.com>
2017-11-19 16:43:43 -0500

    added react-router

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+  BrowserRouter,
+  Route
+} from 'react-router-dom';
+const App = () => (
+  <BrowserRouter>
+    <div className="container">
+
+    </div>
+  </BrowserRouter>
+);
+
+export default App;
