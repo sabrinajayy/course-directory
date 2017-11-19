247c8f544fb33da3978a0909a2515518d03fdeb9
Sabrina John <sabrina.john1990@gmail.com>
2017-11-19 16:52:12 -0500

    added all routes

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,21 @@ import {
   BrowserRouter,
   Route
 } from 'react-router-dom';
+
+import Header from './Header';
+import Home from './Home';
+import About from './About';
+import Teachers from './Teachers';
+import Courses from './Courses';
+
 const App = () => (
   <BrowserRouter>
     <div className="container">
-
+      <Header />
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/teachers" component={Teachers} />
+      <Route path="/courses" component={Courses} />
     </div>
   </BrowserRouter>
 );
