2548e3d760b9fba77f18c6a954d0971fd9205e86
Sabrina John <sabrina.john1990@gmail.com>
2017-11-19 17:23:01 -0500

    added routes to different course lists in courses file

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,17 +1,25 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+
+import CSS from './courses/CSS';
+import HTML from './courses/HTML';
+import JavaScript from './courses/JavaScript';
 
 const Courses = () => (
   <div className="main-content courses">
     <div className="course-header group">
-      <h2>Courses</h2> 
+      <h2>Courses</h2>
       <ul className="course-nav">
-        <li><a href='/courses/html'>HTML</a></li>
-        <li><a href='/courses/css'>CSS</a></li>
-        <li><a href='/courses/javascript'>JavaScript</a></li>
+        <li><NavLink to='/courses/html' activeClassName="active">HTML</NavLink></li>
+        <li><NavLink to='/courses/css' activeClassName="active">CSS</NavLink></li>
+        <li><NavLink to='/courses/javascript' activeClassName="active">JavaScript</NavLink></li>
       </ul>
     </div>
-    
-    {/* Write routes here... */}
+
+    <Route path="/courses/css" component={CSS} />
+    <Route path="/courses/html" component={HTML} />
+    <Route path="/courses/javascript" component={JavaScript} />
   </div>
 );
 
